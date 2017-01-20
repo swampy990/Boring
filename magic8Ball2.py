2ece6c36bf0ae6a1f112fc0671351bba505e0b28
Craig Ballingall <craig.ballingall@gmail.com>
2017-01-20 23:33:00 +0000

    new file

---
+++ magic8Ball2.py
@@ -0,0 +1,12 @@
+import random
+
+messages = ['Fuck',
+            'Shit',
+            'Bugger',
+            'Fart',
+            'Damn',
+            'Bollocks',
+            'Wanker',
+            'Donald']
+
+print(messages[random.randint(0, len(messages) - 1)])
